62de6a157ac60aaa0427dee6e09deb8952d3a04d
Michalenok Marina <mihalenokee@outlook.com>
2023-09-29 13:12:08 +0300

    application.yml, database: r2dbc, migration: flyway, script for 'users' table,

---
+++ src/test/java/com/michalenok/webfluxsecurity/WebfluxsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michalenok.webfluxsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxsecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
